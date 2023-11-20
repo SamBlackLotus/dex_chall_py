729ee41331da91bb8253fd9655ea7803cf4996f6
sam.blacklotus <sam.blacklotus@gmail.com>
2023-11-20 18:16:21 -0300

    Arranging the data

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ arguments = sys.argv
 
 with open("pokemons.json", "r") as source_pokedex:
     data = json.load(source_pokedex)
-    #print(len(data))
     
 for pokemon in data:
-    print(len(pokemon))
+    print(pokemon)
