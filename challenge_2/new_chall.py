4e976bd40460fe867825a0da59062048d8c81b4a
sam.blacklotus <sam.blacklotus@gmail.com>
2023-12-05 16:33:43 -0300

    adjusting CLI commands

--- challenge_2/new_chall.py
+++ challenge_2/new_chall.py
@@ -23,16 +23,21 @@ class Answers(TypedDict):
     
   
    
-def read_file(filepath_1): 
-    with open(filepath_1, "r") as data_1:
-        file_1 = csv.DictReader(data_1, delimiter = ",")
-        p1_pokemons = [row for row in file_1]
-        return p1_pokemons
+def read_file(filepath): 
+    with open(filepath, "r") as data:
+        file = csv.DictReader(data, delimiter = ",")
+        pokemons = [row for row in file]
+        return pokemons
     
 
 def cast_to_int(value):
     return int(value) if value is not None else 0    
 
+def cast_to_bool(value):
+    return True if value == "True" else False 
+
+def cast_to_set():
+    return
 
 def process_pokemons(process_poke1,process_poke2):
     name = {"index": 0, "value": 0}
@@ -45,9 +50,12 @@ def process_pokemons(process_poke1,process_poke2):
     sp_def = {"index": 0, "value": 0}
     speed = {"index": 0, "value": 0}
     generation = {"index": 0, "value": 0}
-    legendary = {"index": 0, "value": 0}
     strongest_p1 = {"index": 0, "value": 0}
     strongest_p2 = {"index": 0, "value": 0}
+    legendary_1 = 0
+    legendary_2 = 0
+    tot_leg_1 = 0
+    tot_leg_2 = 0
     
     
     
@@ -60,7 +68,10 @@ def process_pokemons(process_poke1,process_poke2):
         if attack > strongest_p1["value"]:
             strongest_p1["index"] = index
             strongest_p1["value"] = attack
-            
+        
+        legendary_1 = cast_to_bool(pokemon["Legendary"])     
+        if legendary_1 == True:
+            tot_leg_1 +=1  
         
              
     for index, pokemon in enumerate(process_poke2):
@@ -71,14 +82,22 @@ def process_pokemons(process_poke1,process_poke2):
         if attack > strongest_p2["value"]:
             strongest_p2["index"] = index
             strongest_p2["value"] = attack
+
+        legendary_2 = cast_to_bool(pokemon["Legendary"])
+        if legendary_2 == True:
+            tot_leg_2 +=1              
+            
         
   
     return Answers(
         
         p1_tot_info = p1_total,
         p2_tot_info = p2_total,
-        strgst_p1_info = strongest_p1["index"],
-        strgst_p2_info = strongest_p2["index"]
+        strgst_p1_info = process_poke1[strongest_p1["index"]]["Name"],
+        strgst_p2_info = process_poke2[strongest_p2["index"]]["Name"],
+        leg_1_info = tot_leg_1,
+        leg_2_info = tot_leg_2
+        
          
 )
 
@@ -86,10 +105,10 @@ def show_info(process_pokemons):
     msg = """
                        | Player 1            | Player 2            |
     ------------------ | -------------------- -------------------- |
-    Pokémons           | {p1_tot_info}                 | {p2_tot_info}                 |
-    Strongest Pokémon  | {strgst_p1_info}                  | {strgst_p2_info}                 |
-    Legendaries        |                     |                     |
-    Repeated Pokemons  |                                           |
+    Pokémons           |{p1_tot_info}                  |{p2_tot_info}                  |
+    Strongest Pokémon  |{strgst_p1_info}|{strgst_p2_info}   |
+    Legendaries        |{leg_1_info}                   |{leg_2_info}                   |
+    Repeated Pokemons  |                                           | 
     Different Pokemons |                                           |
     ------------------ | ----------------------------------------- |
     """
@@ -99,7 +118,13 @@ def show_info(process_pokemons):
 def client_helper():
     helper_msg = """
     Hello! Welcome to the Pokedex.
-    We will inform some statistic about your pokemon list.
+    Here you can choose betweem two option, that are:
+    
+    -- info
+    We'll show you comparative information about the two players pokemon lists.
+    
+    -- battle
+    The two players will challenge each other and we'll provide information about the victorious. 
     """
     return print(helper_msg)
 
@@ -118,17 +143,22 @@ def main():
         client_usage()
         quit()
         
-    elif len(sys.argv) > 5:
-       print(f"WARNING! Incorrect amount of arguments.\n{client_usage()}")
-       quit()
+    elif len(sys.argv) == 2:
+        command1 = sys.argv[1]
+        if command1 == "--help":
+            client_helper()
+            quit()
+        else:
+            client_usage()
+            quit()
+            
+    elif len(sys.argv) > 6:
+            print(f"WARNING! Incorrect amount of arguments.\n{client_usage()}")
+            quit()
 
-    command = sys.argv[1]
+    command1 = sys.argv[1]
 
-    if command == "--help":
-        client_helper()
-        quit()
-
-    elif command == "--player1":
+    if command1 == "--player1":
         if len(sys.argv) == 2:
             print(f"WARNING! Incorrect amount of arguments.\n{client_usage()}")
             quit()
@@ -137,20 +167,40 @@ def main():
         if not os.path.exists(filepath_1):
                 print(f"WARNING: File {filepath_1} does not exist.")
                 quit()  
-                
-        filepath_2 = sys.argv[4]
-        if not os.path.exists(filepath_2):
-                 print(f"WARNING: File {filepath_2} does not exist.")
-                 quit()           
-                
-                     
         
-        file_csv_1 = read_file(filepath_1)
-        file_csv_2 = read_file(filepath_2)
+        command2 = sys.argv[3]
+        if command2 == "--player2":
+            if len(sys.argv) == 4:
+                print(f"WARNING! Incorrect amount of arguments.\n{client_usage()}")
+                quit()
+                
+            filepath_2 = sys.argv[4]
+            if not os.path.exists(filepath_2):
+                    print(f"WARNING: File {filepath_2} does not exist.")
+                    quit()              
+        
+    if len(sys.argv) == 5:
+        print(f"WARNING! Incorrect amount of arguments.\n{client_usage()}")
+        quit()
+        
+    file_csv_1 = read_file(filepath_1)
+    file_csv_2 = read_file(filepath_2) 
+            
+    command3 = sys.argv[5]
+    if command3 == "--info":   
         info = process_pokemons(file_csv_1,file_csv_2)
         show_info(info)
     else:
-        print(f"WARNING: This command does not exist.\n{client_usage()}")
+        print(f"WARNING: This command does not exist.")
+        client_usage()
+        quit()
+        
+    if command3 == "--battle":
+        print("Batalha pokemon!")
+    else:
+        print(f"WARNING: This command does not exist.")
+        client_usage()
+        quit()
 
         
 if __name__ == "__main__":
