8591a748ad45124258758c22a67a3e4569c61567
sam.blacklotus <sam.blacklotus@gmail.com>
2023-12-18 13:33:55 -0300

    final final

--- challenge_3/pokedex_crud.py
+++ challenge_3/pokedex_crud.py
@@ -566,6 +566,9 @@ def client_helper():
     The two players will challenge each other, using their three strongest
     pokemon, where the first pokemon to fall decides the winner, and we'll
     show you the battle information.
+    
+    Warning:
+   
     """
     return print(helper_msg)
 
@@ -581,16 +584,33 @@ def client_usage():
         ATENTION:
         
         Where you read <pokemons_1.json> or <pokemons_2.json> 
-        You can use the file format that best suits your needs:
-        acrescentar a explicação que voce pode misturar tipos de arquivos diferentes
+        Notice that the both list don't need to be in the same format, here you can use more than one file
+        fomat at the same time, like a json and a csv list at the same time, it will work as well.
+        You can use the formats that best suits your needs:
         Example --> pokemons_1.json  pokemons_2.json 
                     pokemons_1.csv   pokemons_2.csv
                     pokemons_1.xml   pokemons_2.xml
                     pokemons_1.yaml  pokemons_2.yaml
                     
-        descrever o funcionamento do id
+        For all options you'll need to inform an id, so the function can
+        save the log in a text file, if you don't inform any id number it will be 
+        automatically set as 0.
+    
+        Before using the function you can choose what will be done with the information
+        generated, answering the question that will be displayed: what do you prefer to do? 
         
-        descrever como excluir ou incrementar o arquivo
+        [append|OVERWRITE]
+        
+        append -> This function will increment the file, keeping the information that already
+        exists on the file and simply add the new one, to choose this type any of this:
+        'a','A','Append','append','APPEND'  
+        
+        OVERWRITE -> This option will delete the existing file and create a new one with the
+        generated information, to choose this type any of this: 
+        'o','O','Overwrite','overwrite','OVERWRITE'. 
+        
+        WARNING: If you don't choose an option, and just hit enter it will automatically use 
+        OVERWRITE option. 
         
         
     """
